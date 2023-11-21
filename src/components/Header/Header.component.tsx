2dd3987da4bc720256e3267ec81342be43e62235
Din Ben Elisha <dinbenel@gmail.com>
2023-11-21 20:46:18 +0200

    header links

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -1,5 +1,8 @@
-import { routeName } from '@/constants/routeNames';
 import { NavLink } from 'react-router-dom';
+import PokeBallSvg from '../Svg/PokeBall.svg';
+import { routeName } from '@/constants/routeNames';
+import Avatr from '../Svg/Avatr.svg';
+import Bars from '../Svg/Bars.svg';
 
 const Header = () => {
   const getLinkTitle = (routeName: string) => {
@@ -9,16 +12,30 @@ const Header = () => {
   };
 
   return (
-    <header className='bg-foreground text-background text-xl'>
-      <nav className='flex items-center justify-center gap-4 p-6 max-w-7xl mx-auto'>
-        <NavLink to={routeName.home}>{getLinkTitle(routeName.home)}</NavLink>
-        <NavLink to={routeName.login}>{getLinkTitle(routeName.login)}</NavLink>
-        <NavLink to={routeName.register}>
-          {getLinkTitle(routeName.register)}
-        </NavLink>
-        <NavLink to={routeName.pokemon}>
-          {getLinkTitle(routeName.pokemon)}
-        </NavLink>
+    <header className='text-background text-2xl capitalize bg-foreground p-4'>
+      <nav className='flex items-center justify-between container'>
+        <section className='flex items-center justify-center'>
+          <div>
+            <PokeBallSvg className='h-20' />
+          </div>
+          <span>poke-info</span>
+        </section>
+        <section className='flex items-center justify-center gap-4 me-20'>
+          <NavLink to={routeName.home}>{getLinkTitle(routeName.home)}</NavLink>
+          <NavLink to={routeName.login}>
+            {getLinkTitle(routeName.login)}
+          </NavLink>
+          <NavLink to={routeName.register}>
+            {getLinkTitle(routeName.register)}
+          </NavLink>
+          <NavLink to={routeName.pokemon}>
+            {getLinkTitle(routeName.pokemon)}
+          </NavLink>
+        </section>
+        <section className='bg-background p-1 rounded-full flex items-center justify-center gap-2'>
+          <Avatr className='h-8 w-8 text-foreground' />
+          <Bars className='h-6 w-6 text-foreground' />
+        </section>
       </nav>
     </header>
   );
